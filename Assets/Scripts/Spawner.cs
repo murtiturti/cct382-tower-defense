9b80bdc36151d2003f6cfb986b5500da9bf8d59b
murtiturti <90635908+murtiturti@users.noreply.github.com>
2024-12-11 17:32:33 -0500

    implemented spawner
    not tested

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,19 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Spawner : MonoBehaviour
 {
     #region Parameters
     [SerializeField] private float rate;
-    [SerializeField] private List<GameObject> enemies; // should be list of Mobs when Mob base class is created
+    [SerializeField] private List<GameObject> mobPrefabs; // prefabs
     [SerializeField] private float[] weights;
     [SerializeField] private float minSpawnTime;
     [SerializeField] private float maxSpawnTime;
+    [SerializeField] private int minMobCount;
+    [SerializeField] private int maxMobCount;
+    [SerializeField] private float spawnBaseRate;
+    [FormerlySerializedAs("spawnRate")] [SerializeField] private float spawnIncreaseRate;
     #endregion
 
     private float _timer;
+    private float _spawnTime;
+    private int _waveNumber;
+    
+    private Queue<GameObject> _spawnQueue;
     
     private enum SpawnType {
         Weighted,
@@ -26,16 +36,94 @@ public class Spawner : MonoBehaviour
     private void Awake()
     {
         SetupWeights();
+        _timer = 0f;
+        _waveNumber = 0;
+        _spawnTime = UnityEngine.Random.Range(minSpawnTime, maxSpawnTime);
+        _spawnQueue = new Queue<GameObject>();
     }
 
     private void Update()
     {
         _timer += Time.deltaTime;
+        if (_timer >= _spawnTime)
+        {
+            _timer = 0f;
+            Spawn();
+        }
     }
 
     public void Spawn()
     {
+        if (_spawnQueue.Count != 0)
+        {
+            var mob = _spawnQueue.Dequeue();
+            Instantiate(mob, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            AddMobWave();
+            _waveNumber++;
+        }
+        _spawnTime = UnityEngine.Random.Range(minSpawnTime, maxSpawnTime);
+    }
+
+    private void AddMobWave()
+    {
+        int spawnCount = minMobCount;
+        if (_spawnType == SpawnType.Random)
+        {
+            spawnCount = (int) Math.Clamp(Math.Floor(minMobCount * Math.Pow(spawnBaseRate, _waveNumber * spawnIncreaseRate)), minMobCount, maxMobCount);
+            for (int i = 0; i < spawnCount; i++)
+            {
+                var mob = mobPrefabs[UnityEngine.Random.Range(0, mobPrefabs.Count)];
+                _spawnQueue.Enqueue(mob);
+            }
+        }
+        else if (_spawnType == SpawnType.Weighted)
+        {
+            spawnCount = (int) Math.Clamp(Math.Floor(minMobCount * Math.Pow(spawnBaseRate, _waveNumber * spawnIncreaseRate)), minMobCount, maxMobCount);
+            var adjustedWeights = GetAdjustedWeights();
+            for (int i = 0; i < spawnCount; i++)
+            {
+                var mob = GetRandomMobByWeight(adjustedWeights, adjustedWeights.Sum());
+                _spawnQueue.Enqueue(mob);
+            }
+        }
+        else
+        {
+            //spawn type is levels, come back later
+        }
+
         
+    }
+    
+    private float[] GetAdjustedWeights()
+    {
+        float[] adjustedWeights = new float[weights.Length];
+        for (int i = 0; i < weights.Length; i++)
+        {
+            adjustedWeights[i] = weights[i] * (1 + (_waveNumber * 0.1f));
+        }
+        return adjustedWeights;
+    }
+    
+    private GameObject GetRandomMobByWeight(float[] adjustedWeights, float totalWeight)
+    {
+        float randomValue = UnityEngine.Random.Range(0f, totalWeight);
+        float cumulativeWeight = 0f;
+
+        for (int i = 0; i < adjustedWeights.Length; i++)
+        {
+            cumulativeWeight += adjustedWeights[i];
+
+            if (randomValue <= cumulativeWeight)
+            {
+                return mobPrefabs[i];
+            }
+        }
+
+        // Fallback, in case of float precision issues
+        return mobPrefabs[0];
     }
 
     public void SetSpawnerType(GameMode gameMode)
@@ -62,7 +150,7 @@ public class Spawner : MonoBehaviour
         /*
          * This function sets up the weights array to avoid exceptions pre weight-optimization
          */
-        weights = new float[enemies.Count];
+        weights = new float[mobPrefabs.Count];
         for (int i = 0; i < weights.Length; i++)
         {
             weights[i] = 1f;
