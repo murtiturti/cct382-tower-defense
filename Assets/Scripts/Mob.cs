2a360100a9b05bda222c6fe94f25e77e6f725a34
murtiturti <90635908+murtiturti@users.noreply.github.com>
2024-12-11 19:23:17 -0500

    some mob stuff

--- Assets/Scripts/Mob.cs
+++ Assets/Scripts/Mob.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Mob : MonoBehaviour
 {
+    [SerializeField] private List<int> path;
+    private List<Transform> pathObjects;
+
+    [SerializeField] private float health;
+    [SerializeField] private float speed;
+    [SerializeField] private float damage;
+    
+    //TODO: Add strengths and weaknesses
+    
+    private int _currentPathIndex = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var assistant = FindObjectOfType<PathAssistant>();
+        path = assistant.ChooseRandomPath();
+        var parent = GameObject.Find("Nodes");
+        foreach (Transform child in parent.transform)
+        {
+            pathObjects.Add(child);
+        }
+        transform.position = pathObjects[_currentPathIndex].position;
     }
 }
---
+++ Assets/Scripts/PathAssistant.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathAssistant : MonoBehaviour
+{
+    public List<int> path0;
+    public List<int> path1;
+    public List<int> path2;
+    public List<int> path3;
+    public List<int> path4;
+    public List<int> path5;
+    
+    public List<List<int>> paths;
+
+    private void Awake()
+    {
+        paths = new List<List<int>>();
+        paths.Add(path0);
+        paths.Add(path1);
+        paths.Add(path2);
+        paths.Add(path3);
+        paths.Add(path4);
+        paths.Add(path5);
+    }
+
+    public List<int> ChooseRandomPath()
+    {
+        return paths[UnityEngine.Random.Range(0, paths.Count)];
+    }
+}
