cd120c83a69421cba0ba59c725efd7b8973440cb
Nikunj Shanker <nikunj.shanker@gmail.com>
2024-12-14 14:19:29 -0500

    Hell Yeah Tower Selection

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -1,19 +1,28 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Playables;
 using UnityEngine.Tilemaps;
 
 public class TowerManager : MonoBehaviour
 {
     [SerializeField] private GameObject towerPrefab;
+    [SerializeField] private List<GameObject> towers;
+    [SerializeField] private GameObject towerSelectorObject;
 
     public Tilemap placementTilemap;
     public TileBase validTile;
-    
-    private Grid _grid;
 
+    private TileBase selectedTile;
+    private Vector3Int selectedCellpos;
+
+    private PlayableDirector openSelectorDir;
+    private PlayableDirector closeSelectorDir;
+
+    private Grid _grid;
     private Camera _mainCam;
+
+    private bool selectorOpen;
 
     [SerializeField] private IntVariable playerMoney;
 
@@ -24,33 +33,65 @@ public class TowerManager : MonoBehaviour
 
     private void Start()
     {
+        selectorOpen = false;
         _grid = placementTilemap.layoutGrid;
+
+        openSelectorDir = GameObject.Find("Open Selector Timeline").GetComponent<PlayableDirector>();
+        closeSelectorDir = GameObject.Find("Close Selector Timeline").GetComponent<PlayableDirector>();
+    }
+
+    public void placeTower(int tower_type)
+    {
+        GameObject towerPrefab = towers[tower_type];
+        var tower = towerPrefab.GetComponent<Tower>();
+
+        if (playerMoney.Value >= tower.cost && selectorOpen)
+        {
+            Vector3 towerPos = _grid.GetCellCenterWorld(selectedCellpos);
+
+            Instantiate(towerPrefab, towerPos, Quaternion.identity);
+
+            placementTilemap.SetTile(selectedCellpos, null);
+            playerMoney.Value -= tower.cost;
+
+            closeSelectorMenu();
+        }
+    }
+
+    private void openSelectorMenu(Vector3 mousePos)
+    {
+        openSelectorDir.Play();
+        towerSelectorObject.transform.position = new Vector3(mousePos.x, mousePos.y, towerSelectorObject.transform.position.z);
+        selectorOpen = true;
+    }
+
+    private void closeSelectorMenu()
+    {
+        closeSelectorDir.Play();
+        selectorOpen = false;
+        selectedTile = null;
+        selectedCellpos = Vector3Int.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
-            var tower = towerPrefab.GetComponent<Tower>();
-            if (tower != null)
+            if (!selectorOpen)
             {
-                if (playerMoney.Value >= tower.cost)
-                {
-                    Vector3 mousePos = _mainCam.ScreenToWorldPoint(Input.mousePosition);
-                    Vector3Int cellPosition = _grid.WorldToCell(mousePos);
-            
-                    TileBase clickedTile = placementTilemap.GetTile(cellPosition);
-                    if (clickedTile == validTile)
-                    {
-                        Vector3 towerPos = _grid.GetCellCenterWorld(cellPosition);
+                Vector3 mousePos = _mainCam.ScreenToWorldPoint(Input.mousePosition);
+                selectedCellpos = _grid.WorldToCell(mousePos);
+                selectedTile = placementTilemap.GetTile(selectedCellpos);
 
-                        Instantiate(towerPrefab, towerPos, Quaternion.identity);
-                
-                        placementTilemap.SetTile(cellPosition, null);
-                        playerMoney.Value -= tower.cost;
-                    }
+                if (selectedTile)
+                {
+                    openSelectorMenu(mousePos);
                 }
+            }
+            else
+            {
+                closeSelectorMenu();
             }
         }
     }
